891a0e91b6b5b96fb6090637cbdd4670c2735d1b
Ketan Lambat <ketan.lambat@ymail.com>
2020-02-20 12:39:22 +0530

    apps created

--- drive/settings.py
+++ drive/settings.py
@@ -35,7 +35,10 @@ INSTALLED_APPS = [
 	'django.contrib.sessions',
 	'django.contrib.messages',
 	'django.contrib.staticfiles',
+	'base',
 	'registration',
+	'accounts',
+	'drive_data',
 ]
 
 MIDDLEWARE = [
---
+++ drive_data/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class DriveDataConfig(AppConfig):
+    name = 'drive_data'
