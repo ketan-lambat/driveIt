73eb60725a77e7f5632c5a2907503a321ff61136
Saideepthi123 <52497119+Saideepthi123@users.noreply.github.com>
2020-02-25 11:39:08 +0530

    accounts/urls

--- urls.py
+++ urls.py
@@ -1,8 +1,10 @@
-
-from django.contrib import admin
-from django.urls import path,include
-
+from django.urls import path
+from .import views
+from django.contrib.auth.views import LoginView,LogoutView
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('accounts/',include('accounts.urls'))
+    path('',views.indexView, name='home'),
+    path('dashboard/',views.dashboardView, name="dashboard"),
+    path('login/',LoginView.as_view(),name="login_url"),
+    path('register/',views.registerView, name="register_url"),
+    path('logout/',LogoutView.as_view(next_page='dashboard'),name="logout"),
 ]
