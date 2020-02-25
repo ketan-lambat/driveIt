ba759571ef605acc3ed994bf31c025b40b9c63c1
Saideepthi123 <52497119+Saideepthi123@users.noreply.github.com>
2020-02-25 11:47:17 +0530

    Add files via upload

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+def indexView(request):
+    return render(request,'index.html')
+@login_required
+
+def dashboardView(request):
+    return render(request,'dashboard.html')
+
+def registerView(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login_url')
+    else:
+        form = UserCreationForm()
+
+    return render(request,'registration/register.html',{'form':form})
+
+
+
