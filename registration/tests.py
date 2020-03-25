234d96616cff2e7626e8e96dbb9fb065864f6703
Ketan Lambat <ketan.lambat@ymail.com>
2020-03-25 10:43:28 +0530

    Testing Started
    Signed-off-by: Ketan Lambat <ketan.lambat@ymail.com>

--- registration/tests.py
+++ registration/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from django.urls import resolve
+from django.http import HttpRequest
+from .views import index_view
 
-# Create your tests here.
+
+class RegistrationPageTest(TestCase):
+
+	def test_root_url(self):
+		response = self.client.get('/')
+		self.assertEqual(response.status_code, 200)
+
+	def test_root_url_resolves_to_home_page(self):
+		found_url = resolve('/')
+		self.assertEqual(found_url.func, index_view)
+
+	def test_root_title(self):
+		response = self.client.get('/')
+		self.assertContains(response, "<title>Simple Auth System</title>")
+		self.assertTemplateUsed(response, "registration/index.html")
+
