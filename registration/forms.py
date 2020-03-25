86ab5e81b9648793aa68513e35622b3807e0b822
Ashutosh Chauhan <masterashu@live.in>
2020-03-25 13:42:36 +0530

    updated user creation form
    Co-authored-by: HARICHANDANAAKULA <harichandana.a18@iiits.in>

--- registration/forms.py
+++ registration/forms.py
@@ -1,19 +1,62 @@
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
+from django import forms
+from django.db import models
+from registration.models import User
+from django.contrib.auth import password_validation
+from django.utils.translation import gettext, gettext_lazy as _
 
 
-class CreateUserForm(UserCreationForm):
-
+class CreateUserForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ["username", "first_name", "last_name", "email", "password1", "password2"]
 
+    error_messages = {
+        'password_mismatch': _('The two password fields didn’t match.'),
+    }
+
+    password1 = forms.CharField(
+        label=_("Password"),
+        strip=False,
+        widget=forms.PasswordInput(attrs={'autocomplete': 'new-password'}),
+        help_text=password_validation.password_validators_help_text_html(),
+    )
+
+    password2 = forms.CharField(
+        label=_("Password confirmation"),
+        widget=forms.PasswordInput(attrs={'autocomplete': 'new-password'}),
+        strip=False,
+        help_text=_("Enter the same password as before, for verification."),
+    )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if self._meta.model.USERNAME_FIELD in self.fields:
+            self.fields[self._meta.model.USERNAME_FIELD].widget.attrs['autofocus'] = True
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError(
+                self.error_messages['password_mismatch'],
+                code='password_mismatch',
+            )
+        return password2
+
+    def _post_clean(self):
+        super()._post_clean()
+        # Validate the password after self.instance is updated with form data
+        # by super().
+        password = self.cleaned_data.get('password2')
+        if password:
+            try:
+                password_validation.validate_password(password, self.instance)
+            except forms.ValidationError as error:
+                self.add_error('password2', error)
+
     def save(self, commit=True):
         user = super().save(commit=False)
-        user.first_name = self.cleaned_data['first_name']
-        user.last_name = self.cleaned_data['last_name']
-        user.email = self.cleaned_data['email']
-        # user.set_password(self.cleaned_data["password1"])
+        user.set_password(self.cleaned_data["password1"])
         if commit:
             user.save()
         return user
