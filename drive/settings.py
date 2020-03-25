4ab54b1393fea3399f62d38f914a2417712926d9
Ashutosh Chauhan <masterashu@live.in>
2020-03-25 11:52:16 +0530

    making absolute path for templates

--- drive/settings.py
+++ drive/settings.py
@@ -64,7 +64,7 @@ ROOT_URLCONF = 'drive.urls'
 TEMPLATES = [
 	{
 		'BACKEND': 'django.template.backends.django.DjangoTemplates',
-		'DIRS': ['templates/'],
+		'DIRS': [os.path.join(BASE_DIR, 'templates/')],
 		'APP_DIRS': True,
 		'OPTIONS': {
 			'context_processors': [
